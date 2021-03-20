f2d1e725b71a6bc82227f162bdffe1dd5357d8c4
leslier4 <leslier4@wwu.edu>
2021-03-19 19:16:40 -0700

    serve update

--- serve.js
+++ serve.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const path = require('path');
 
 // app.get('/*', (req, res) => {
@@ -14,6 +14,6 @@ const path = require('path');
 
 app.get('/services', (req, res) =>{
     res.send(`<h1>Hello world</h1>`);
-})
+});
 
 app.listen(port);
