a45cab4459203572dcfe3dcccef52ee97561dd80
leslier4 <leslier4@wwu.edu>
2021-03-19 18:39:19 -0700

    testing server

--- serve.js
+++ serve.js
@@ -3,12 +3,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 
-app.get('/projects/marquee/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public_html/projects/marquee/index.html'), (err)=>{
-        if(err){
-            res.status(500).send("i got here");
-        }
-    });
+app.get('/*', (req, res) => {
+    // res.sendFile(path.join(__dirname, '../public_html/projects/marquee/index.html'), (err)=>{
+    //     if(err){
+    //         res.status(500).send("i got here");
+    //     }
+    /*});*/
+    res.send("hello there");
 });
 
 app.listen(port);
